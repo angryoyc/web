af9a59739c76b0729e60f26a07c2572fdc89f42b
Serg A. Osipov <serg.osipov@gmail.com>
2017-12-15 23:27:28 +1000

    modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -13,194 +13,151 @@ nock('http://stroka.settv.ru').post('/api/fr/test').times(4).reply(200, { error:
 describe('Web', function(){
 
 	describe('xget', function(){
-		it('should return object with body and response', function(done){
-			web.xget('http://stroka.settv.ru/api/fr/test')
-			.then(
-				function(result){
-					result.should.type('object');
-					result.response.should.type('object');
-					result.response.statusCode.should.eql(200);
-					eval('(' + result.body +')') .should.eql({ error: 0, message: 'Ok', data: { ok: true } });
-					done();
-				},
-				function(err){
-					console.log('RESULT', err)
-					return done(err);
-				}
-			).catch(done);
+		it('should return object with body and response', async function(done){
+			try{
+				let result = await web.xget('http://stroka.settv.ru/api/fr/test');
+				result.should.type('object');
+				result.response.should.type('object');
+				result.response.statusCode.should.eql(200);
+				eval('(' + result.body +')') .should.eql({ error: 0, message: 'Ok', data: { ok: true } });
+				done();
+			}catch(err){
+				done(err);
+			};
 		})
 	});
 
 	describe('xget', function(){
-		it('should return object with body and response', function(done){
-			web.xget('http://admin:123@stroka.settv.ru/api/fr/test')
-			.then(
-				function(result){
-					result.should.type('object');
-					result.response.should.type('object');
-					result.response.statusCode.should.eql(200);
-					eval('(' + result.body +')') .should.eql({ error: 0, message: 'Ok', data: { ok: true } });
-					done();
-				},
-				function(err){
-					console.log('RESULT', err)
-					return done(err);
-				}
-			).catch(done);
-		})
+		it('should return object with body and response', async function(done){
+			let result = await web.xget('http://admin:123@stroka.settv.ru/api/fr/test');
+			try{
+				result.should.type('object');
+				result.response.should.type('object');
+				result.response.statusCode.should.eql(200);
+				eval('(' + result.body +')') .should.eql({ error: 0, message: 'Ok', data: { ok: true } });
+				done();
+			}catch(err){
+				done(err);
+			};
+		});
 	});
 
 	describe('get', function(){
-		it('should return right response body', function(done){
-			web.get('http://stroka.settv.ru/api/fr/test')
-			.then(
-				function(result){
-					eval('(' + result +')') .should.eql({ error: 0, message: 'Ok', data: { ok: true } });
-					done();
-				},
-				function(err){
-					console.log('RESULT', err)
-					return done(err);
-				}
-			).catch(done);
-		})
+		it('should return right response body', async function(done){
+			try{
+				let result = await web.get('http://stroka.settv.ru/api/fr/test');
+				eval('(' + result +')') .should.eql({ error: 0, message: 'Ok', data: { ok: true } });
+				done();
+			}catch(err){
+				done(err);
+			};
+		});
 	});
 
 	describe('get_json', function(){
-		it('should return right answer', function(done){
-			web.get_json('http://stroka.settv.ru/api/fr/test')
-			.then(
-				function(result){
-					result.should.eql({ error: 0, message: 'Ok', data: { ok: true } });
-					done();
-				},
-				function(err){
-					return done(err);
-				}
-			).catch(done);
-		})
+		it('should return right answer', async function(done){
+			try{
+				let result = await web.get_json('http://stroka.settv.ru/api/fr/test');
+				result.should.eql({ error: 0, message: 'Ok', data: { ok: true } });
+				done();
+			}catch(err){
+				done(err);
+			};
+		});
 	});
 
 	describe('get_file', function(){
-		it('should return right answer', function(done){
+		it('should return right answer', async function(done){
 			web.setconf({tempdir:'/tmp'});
-			web.get_file('http://stroka.settv.ru/file')
-			.then(
-				function(result){
-
-					should.exist(result);
-					result.should.type('object');
-					result.file.should.type('string');
-
-					if (!fs.existsSync(result.file)) {
-						throw ("Downloaded file is not exists");
-					};
-
-					fs.unlink(result.file, (err)=>{});
-					done();
-				},
-				function(err){
-					return done(err);
-				}
-			).catch(done);
-		})
+			try{
+				let result = await web.get_file('http://stroka.settv.ru/file');
+				should.exist(result);
+				result.should.type('object');
+				result.file.should.type('string');
+				if (!fs.existsSync(result.file)) {
+					throw ("Downloaded file is not exists");
+				};
+				fs.unlink(result.file, (err)=>{});
+				done();
+			}catch(err){
+				return done(err);
+			};
+		});
 	});
 
 
 	describe('get_file_with_redirect', function(){
-		it('should return right answer', function(done){
+		it('should return right answer', async function(done){
 			web.setconf({tempdir:'/tmp'});
-			web.get_file_with_redirect('http://stroka.settv.ru/file')
-			.then(
-				function(result){
-					should.exist(result);
-					result.should.type('object');
-					result.file.should.type('string');
-					if (!fs.existsSync(result.file)) {
-						throw ("Downloaded file is not exists");
-					};
-					fs.unlink(result.file, (err)=>{});
-					done();
-				},
-				function(err){
-					return done(err);
-				}
-			).catch(done);
-		})
+			try{
+				let result = await web.get_file_with_redirect('http://stroka.settv.ru/file');
+				should.exist(result);
+				result.should.type('object');
+				result.file.should.type('string');
+				if (!fs.existsSync(result.file)) {
+					throw ("Downloaded file is not exists");
+				};
+				fs.unlink(result.file, (err)=>{});
+				done();
+			}catch(err){
+				return done(err);
+			};
+		});
 	});
 
 
 
 	describe('xpost', function(){
-		it('should return object', function(done){
-			web.xpost('http://stroka.settv.ru/api/fr/test', "")
-			.then(
-				function(result){
-					result.should.type('object');
-					result.response.should.type('object');
-					result.response.statusCode.should.eql(200);
-					eval('(' + result.body +')').should.eql({ error: 0, message: 'Ok', data: { ok: true } });
-					done();
-				},
-				function(err){
-					console.log('RESULT', err)
-					return done(err);
-				}
-			).catch(done);
-		})
+		it('should return object', async function(done){
+			try{
+				let result = await web.xpost('http://stroka.settv.ru/api/fr/test', "");
+				result.should.type('object');
+				result.response.should.type('object');
+				result.response.statusCode.should.eql(200);
+				eval('(' + result.body +')').should.eql({ error: 0, message: 'Ok', data: { ok: true } });
+				done();
+			}catch(err){
+				return done(err);
+			};
+		});
 	});
 
 
 	describe('post', function(){
-		it('should return right answer', function(done){
-			web.post('http://stroka.settv.ru/api/fr/test', "")
-			.then(
-				function(result){
-					eval('(' + result +')') .should.eql({ error: 0, message: 'Ok', data: { ok: true } });
-					done();
-				},
-				function(err){
-					console.log('RESULT', err)
-					return done(err);
-				}
-			).catch(done);
-		})
+		it('should return right answer', async function(done){
+			try{
+				let result = await web.post('http://stroka.settv.ru/api/fr/test', "");
+				eval('(' + result +')') .should.eql({ error: 0, message: 'Ok', data: { ok: true } });
+				done();
+			}catch(err){
+				return done(err);
+			};
+		});
 	});
 
-
-
 	describe('post_json', function(){
-		it('should return right answer', function(done){
-			web.post_json('http://stroka.settv.ru/api/fr/test', {})
-			.then(
-				function(result){
-					result.should.eql({ error: 0, message: 'Ok', data: { ok: true } });
-					done();
-				},
-				function(err){
-					return done(err);
-				}
-			).catch(done);
+		it('should return right answer', async function(done){
+			try{
+				let result = await web.post_json('http://stroka.settv.ru/api/fr/test', {});
+				result.should.eql({ error: 0, message: 'Ok', data: { ok: true } });
+				done();
+			}catch(err){
+				return done(err);
+			};
 		})
 	});
 
 	describe('api', function(){
-		it('should return right answer', function(done){
-			web.api('http://stroka.settv.ru/api/fr/test', {})
-			.then(
-				function(result){
-					result.should.eql({ ok: true });
-					//-result.should.eql({ error: 0, message: 'Ok', data: { ok: true } });
-					done();
-				},
-				function(err){
-					return done(err);
-				}
-			).catch(done);
+		it('should return right answer', async  function(done){
+			try{
+				let result = await web.api('http://stroka.settv.ru/api/fr/test', {});
+				result.should.eql({ ok: true });
+				//-result.should.eql({ error: 0, message: 'Ok', data: { ok: true } });
+				done();
+			}catch(err){
+				return done(err);
+			};
 		})
 	});
 
-
-/*
-*/
 });
